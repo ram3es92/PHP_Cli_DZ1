b3ac4ccb3f7517c34ed5983b9ed494451bfcdc4e
Роман Князев <ram3es92@mail.ru>
2025-02-02 14:53:45 +0300

    Изменения в Command/SEC.php

--- app/Commands/SaveEventCommand.php
+++ app/Commands/SaveEventCommand.php
@@ -11,10 +11,7 @@ use App\Models\Event;
 class SaveEventCommand extends Command
 {
 
-    protected Application $app;
-
     public function __construct(Application $app)
-
     {
 
         $this->app = $app;
