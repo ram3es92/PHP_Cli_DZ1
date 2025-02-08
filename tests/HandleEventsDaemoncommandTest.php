84936543026efb8c26302ebb177bec2a5afcccf6
Роман Князев <ram3es92@mail.ru>
2025-02-08 21:03:52 +0300

    сделал изменения в тесте демона

--- tests/HandleEventsDaemoncommandTest.php
+++ tests/HandleEventsDaemoncommandTest.php
@@ -3,6 +3,17 @@
 use PHPUnit\Framework\TestCase;
 
 class HandleEventsDaemonCommandTest extends TestCase
-{
 
+{
+    public function testGetCurrentTime()
+        {
+            $handleEventsDaemonCommandTest = new \App\Commands\HandleEventsDaemonCommand(new \App\Application(dirname(__DIR__)));
+    
+            self::assertEquals(
+                $handleEventsDaemonCommandTest->getCurrentTime(),
+                [
+
+                ]
+            );
+        }
 }
